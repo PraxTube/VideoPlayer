e78aad78d2888a7d1f7d784328573d30c1aebc8b
PraxRax <62686404+PraxRax@users.noreply.github.com>
2022-07-10 18:15:56 +0200

    Added comments for VectorClasses

--- VectorClasses.py
+++ VectorClasses.py
@@ -1,14 +1,18 @@
 class IntVector2:
+    """Provide the functionality of a Vector2 for integer."""
     def __init__(self, x, y):
+        """Initialise x and y if they are integer."""
         if not isinstance(x, int) or not isinstance(y, int):
             raise ValueError("Numbers need to be of type integer", (x, y))
         self.x = x
         self.y = y
 
     def __str__(self):
+        """Return a string with the format (x, y)."""
         return "({}, {})".format(self.x, self.y)
 
     def __getitem__(self, i):
+        """Provide the ability to index x and y through indexing."""
         if type(i) != int:
            raise TypeError("Index not an integer", i)
         if i == 0:
@@ -18,11 +22,13 @@ class IntVector2:
         raise IndexError("Index outside of range", i)
 
     def __add__(self, other):
+        """Add two IntVector2 instances together, else raise error."""
         if not isinstance(other, IntVector2):
             raise TypeError("Cannot \'{}\' type \'{}\' with \'{}\'".format("+", type(self), type(other)), other)
         return IntVector2(self.x + other.x, self.y + other.y)
 
     def __mul__(self, other):
+        """Calculate dot-product or scale IntVector2, depending on other."""
         if isinstance(other, IntVector2):
             return self.x * other.x + self.y * other.y
         if isinstance(other, int):
@@ -32,6 +38,7 @@ class IntVector2:
         raise TypeError("Cannot \'{}\' type \'{}\' with \'{}\'".format("*", type(self), type(other)), other)
 
     def __rmul__(self, other):
+        """Calculate dot-product or scale IntVector2, depending on other."""
         if isinstance(other, IntVector2):
             return self.x * other.x + self.y * other.y
         if isinstance(other, int):
@@ -41,4 +48,5 @@ class IntVector2:
         raise TypeError("Cannot \'{}\' type \'{}\' with \'{}\'".format("*", type(self), type(other)), other)
 
     def copy(self):
+        """Copy self by creating a new instance with same values."""
         return IntVector2(self.x, self.y)
